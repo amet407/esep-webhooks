4480b9737be05371ce2a23da11cfa84d74a805bd
Ametoje Virdi <99097068+amet407@users.noreply.github.com>
2024-11-10 23:58:49 -0500

    Changed parameter in lambda function

--- EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2dceec3d4fd375a387b9acbacd74750bfc9568f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7afc4337022b81835df1052bccd2ffad53b667")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
